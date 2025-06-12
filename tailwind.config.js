6c63dfce02d39db201deb212d124d547b1c68a4e
Ren Yamazaki <r26763041@gmail.com>
2025-06-13 01:24:47 +0900

    ナビゲーションバーの作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#ebf8ff",
-          300: "#90cdf4",
-          500: "#4299e1"
-        }
-      }
+        body: "#050505",
+        "selected-text": "#a3a3ff",
+        theme: "#5c318c",
+      },
     },
+    fontFamily: {
+      notosansjp: ["Noto Sans JP", "sans-serif"],
+    }
   },
   plugins: [],
 }
